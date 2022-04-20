79ccba665916c10c13849b5f8ffe9c1215093911
Phil.Xing <phil.xing@adlinktech.com>
2022-04-21 00:15:30 +0800

    Add tools.

--- dediprog.bat
+++ dediprog.bat
@@ -1,9 +1,11 @@
 @echo off
 setlocal
 Path="C:\Program Files (x86)\DediProg\SF Programmer";%PATH%
-UsbRelayCh340 COM4 NO
+@REM UsbRelayCh340 COM4 NO
+UsbRelayHid NO
 TIMEOUT /T 3
 dpcmd.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
-UsbRelayCh340 COM4 NC
+@REM UsbRelayCh340 COM4 NC
+UsbRelayHid NC
 endlocal
 echo on
